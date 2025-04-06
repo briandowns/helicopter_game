cba4b3eb15842dab220a1245a5f349869e3ca9ab
Brian Downs <brian.downs@gmail.com>
2025-04-06 12:52:13 -0700

    more
    Signed-off-by: Brian Downs <brian.downs@gmail.com>

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <raylib.h>
 #include <raymath.h>
 
+#define RAYTMX_IMPLEMENTATION
+#include "raytmx.h"
+
 #define MAX_FRAME_SPEED 15
 #define MIN_FRAME_SPEED  1
 
@@ -15,9 +18,9 @@
 void
 DrawTime()
 {
-    double gt = GetTime();
-    int seconds = (int)gt%60;
-    int minutes = (int)gt/60;
+    double now = GetTime();
+    int seconds = (int)now%60;
+    int minutes = (int)now/60;
 
     DrawText(TextFormat("Elapsed Time: m %i s %i", minutes, seconds), 0, 0, 30, WHITE);
 }
@@ -25,7 +28,7 @@ DrawTime()
 int
 main(void)
 {
-    InitWindow(1200, 900, "Helicopter");
+    InitWindow(1280, 900, "Helicopter");
     InitAudioDevice();
 
     // spritesheet
@@ -33,6 +36,10 @@ main(void)
     // height: 320
     // sprite: w 226 x h 80
     Texture2D helicopter_texture = LoadTexture("assets/helicopter-spritesheet.png");
+
+    Texture2D background = LoadTexture("assets/background/Layers/sky.png");
+    // Texture2D midground = LoadTexture("resources/cyberpunk_street_midground.png");
+    // Texture2D foreground = LoadTexture("resources/cyberpunk_street_foreground.png");
 
     Sound sound = LoadSound("");
     Music music = LoadMusicStream("assets/sound/airwolf.mp3");
@@ -46,13 +53,28 @@ main(void)
     int frames_counter = 0;
     int frames_speed = 8; 
 
+    float scrollingBack = 0.0f;
+
     SetTargetFPS(FPS);
 
-    Camera2D camera = {0};
-    camera.target = position;
-    camera.offset = (Vector2){ 1200/2.0f, 900/2.0f };
+    const float panSpeed = 150.0f;
+
+    // Camera2D camera = {0};
+    // camera.target = position;
+    // camera.offset = (Vector2){ 1280/2.0f, 900/2.0f };
+    // camera.rotation = 0.0f;
+    // camera.zoom = 1.0f;
+
+    const char* tmx = "assets/helicopter.tmx";
+    TmxMap* map = LoadTMX(tmx);
+
+    Camera2D camera;
+    camera.zoom = 6.0f;
+    camera.target.x = (float)(map->width * map->tileWidth) / 2.0f;
+    camera.target.y = (float)(map->height * map->tileHeight) / 2.0f;
+    camera.offset.x = (float)1280 / 2.0f;
+    camera.offset.y = (float)900 / 2.0f;
     camera.rotation = 0.0f;
-    camera.zoom = 1.0f;
 
     bool is_in_menu = true;
 
@@ -69,6 +91,10 @@ main(void)
         //         is_in_menu = true;
         //     }
         // }
+
+        scrollingBack -= 0.1f;
+
+        if (scrollingBack <= -background.width*2) scrollingBack = 0;
 
         if (IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_M)) {
             // TODO: look into volume adjustment instead
@@ -91,11 +117,13 @@ main(void)
             if (frame_rec.width < 0) {
                 frame_rec.width = -frame_rec.width;
             }
+            camera.target.x += panSpeed * GetFrameTime();
         } else if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) {
             helicopter_velocity.x = -HELI_SPEED;
             if (frame_rec.width > 0) {
                 frame_rec.width = -frame_rec.width;
             }
+            camera.target.x -= panSpeed * GetFrameTime();
         } else {
             helicopter_velocity.x = 0;
         }
@@ -126,41 +154,29 @@ main(void)
         }
 
         camera.target = position;
-        camera.offset = (Vector2){ 1200/2.0f, 900/2.0f };
+        camera.offset = (Vector2){ 1280/2.0f, 900/2.0f };
         float minX = 1000, minY = 1000, maxX = -1000, maxY = -1000;
 
         // draw section
         BeginDrawing();
-            if (is_in_menu) {
-                ClearBackground(RAYWHITE);
-                //DrawText("Helicopter", 10, 20, 50, WHITE);
-                DrawTime();
-                //DrawText(TextFormat("Elapsed Time: %d s", GetFrameTime()), 10, 20, 50, WHITE);
-            }
+            ClearBackground(RAYWHITE);
+
+            DrawTexture(background, 1280/2 - background.width/2, 900/2 - background.height/2, WHITE);
             
-            ClearBackground(BLACK);
+            BeginMode2D(camera);
+            {
+                //AnimateTMX(map);
+                DrawTMX(map, &camera, 0, 0, WHITE);
+            }
+            EndMode2D();
+            DrawTime();
 
-            //DrawTexture(helicopter, 80, 71, WHITE);
-
-            //DrawRectangleLines(80, 71, helicopter.width, helicopter.height, LIME);
-            //DrawRectangleLines(80 + (int)helicopter.dest_rect.x, 80 + (int)helicopter.dest_rect.y, (int)helicopter.dest_rect.width, (int)helicopter.dest_rect.height, RED);
-
-            //DrawText("FRAME SPEED: ", 165, 210, 10, DARKGRAY);
-            //DrawText(TextFormat("%02i FPS", frames_speed), 575, 210, 10, DARKGRAY);
-            //DrawText("PRESS RIGHT/LEFT KEYS to CHANGE SPEED!", 290, 240, 10, DARKGRAY);
-
-            // for (int i = 0; i < MAX_FRAME_SPEED; i++)
-            // {
-            //     if (i < frames_speed) DrawRectangle(250 + 21*i, 205, 20, 20, RED);
-            //     DrawRectangleLines(250 + 21*i, 205, 20, 20, MAROON);
-            // }
-
-            //DrawTextureRec(helicopter_texture, frame_rec, position, WHITE);
-            DrawTexturePro(helicopter_texture, frame_rec, frame_rec, (Vector2){0, 0}, WHITE);
-
+            DrawTextureRec(helicopter_texture, frame_rec, position, WHITE);
         EndDrawing();
     }
 
+    UnloadTMX(map);
+    UnloadTexture(background);
     UnloadTexture(helicopter_texture);
     StopMusicStream(music);
     CloseAudioDevice();
